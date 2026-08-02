44c0c6acfa18d713cc9e5d695bb99a4952a3340a
johnpetedoe <johnpetedoe@proton.me>
2026-08-02 00:14:21 +0000

    updates

--- srcs/main.c
+++ srcs/main.c
@@ -94,7 +94,7 @@ void format_time (int t, int interval) {
     }
 
     f_putnbr ( time );
-    f_putstr ( " : ");
+    f_putchar ( ':');
 
     if (t % interval < 10) {
         f_putchar('0');
@@ -115,19 +115,84 @@ int main (int argc, char **argv) {
     int i;
     int n;
     int interval;
+    int hour;
+    char *labels[5] = { "AC", "FM", "LB", "MS"};
 
     interval = 60;
     i = 0;
     n = 12 * interval;
     
-    while (i < n) {
+    while (i < n) 
+    {
         format_time(i, interval);
+        // ()
+        // (:= () ('(/ ([1] (p| (m|()) ) ) ([2] (p| (m| ()))) )) )
+
+        //carrier languages vs non-carrier languages
+
+        //{\displaystyle \{P\}C\{Q\}}
+        // (=> P (=> (m (M C)) Q))
+        // (=> P (= (m (M C_0)) Q))
+
+        // proposal for naming convention of recursive.c using hoar logic
+        // (=> P (= (m (M () )) Q))
+        // (:=
+        //      (=> P (= (m (M () )) Q))
+        //      (=> P (= (m (M (=> P (= (m (M () )) Q)) )) Q))
+        // )
+        // (:= 
+        //      (=> P (= (m (M (=> P (= (m (M () )) Q)) )) Q))
+        //      (=> P (= (m (M (=> P (= (m (M (=> P (= (m (M (=> P (= (m (M () )) Q)) )) Q)) )) Q)) )) Q))
+        // )
         
-        if (((i / interval) <= 8) && ((i / interval) > 6)) {
-            f_putstr("\tAC");
+        // this requires every namable instruction to be testable.
+
+        // what of variables?
+
+        // (=> (= () 1) (= (m (M (+ 2 ()))) 3) )
+        // Here, with P as (= () 1), Q as 3, and C as (+ 2 ()) 
+        //      We have named, not C in it's entirety, but an abstraction involved by C.
+
+        // The formula may be expanded by one recurrence as follows:
+        // (=> (= (=> (= () 1) (= (m (M (+ 2 ()))) 3) ) 1) (= (m (M (+ 2 (=> (= () 1) (= (m (M (+ 2 ()))) 3) )))) 3) )
+        
+        // This may be informally translated as: That which when added to 2 and executed by the machine, M, returns 3.
+        // 
+        // This formulation neglects the definition of Q from the Hoar logic using it as an output of a finite state machine instead of an assertion
+
+        // There are two ways to re-accomodate the definition of Q from the Hoar logic:
+        // 0. Use the equality involving the succession of M as the definition of Q such that: (= Q "(= (m (M C)) a)").
+        //      Now Q has taken the form of an assertion describing the sucession of the state of the Machine, M, given the input, C.
+        //      
+        //      That means the entire name/formula takes the form: (=> P Q) Where Q abstracts the statement of equality involving C.
+        //      Although this form only uses the *precondition* and *postcondition*, the *command* is referenced within the post-condition.                    
+        
+        // 1. Re-Structure our formula: 
+        //      I've forgotten the second
+        
+        hour = i / interval;
+
+        f_putchar('\t');
+        if ((hour <= 7) && (hour > 6)) 
+        {
+            f_putstr(labels[0]);
+            f_putchar(' ');
+            f_putstr(labels[0]);
+        } 
+        else if ((hour < 9) && (hour > 7)) 
+        {
+            f_putstr(labels[0]);
+            f_putchar(' ');
+            f_putstr(labels[2]);
+        } 
+        else if (((hour >= 9) && (hour < lb_close))) 
+        {
+            f_putchar(labels[2]);
+            f_putchar(' ');
+            f_putstr(labels[])
+            i++;
         }
-        f_putchar('\n');
-        i++;
+        */
     }
 
     return (0);
